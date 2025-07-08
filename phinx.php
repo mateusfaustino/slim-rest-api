0534d6ece2d3b75525edcfb7f607ddb0b9928fe4
mateus <mateusfaustino2024@gmail.com>
2025-07-08 20:49:50 -0300

    Create migrations

---
+++ phinx.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/database/migrations',
+        'seeds'      => '%%PHINX_CONFIG_DIR%%/database/seeds',
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+        'development' => [
+            'adapter' => 'mysql',
+            'host' => '127.0.0.1',
+            'name' => 'nome_do_banco',
+            'user' => 'root',
+            'pass' => 'senha',
+            'port' => '3306',
+            'charset' => 'utf8mb4',
+        ],
+    ],
+];
